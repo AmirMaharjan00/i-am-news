ea68ae0e0e3e78414371678df58522f995e27ec0
jorasmhr <sarojmhr28@gmail.com>
2026-02-21 14:26:58 +0545

    toggle button

--- inc/customizer/controller/src/components/toggle-button.js
+++ inc/customizer/controller/src/components/toggle-button.js
@@ -1,20 +1,43 @@
 const { useState } = wp.element,
     { RadioControl, Tooltip, FormToggle } = wp.components
 
-export const ToggleButtonComponent = () => {
-    const [ isChecked, setChecked ] = useState( true );
+import { IanControlHead } from './components'
 
-    return (
-        <FormToggle
-            checked={ isChecked }
-            onChange={ () => setChecked( ( state ) => ! state ) }
+export const ToggleButtonComponent = ( props ) => {
+    const { label, description, setting } = props,
+        [ value, setValue ] = useState( setting.get() )
+    
+    /**
+     * Handle change
+     * 
+     * @since 1.0.0
+     */
+    const handleChange = () => {
+        setValue( ! value ) // this only change the currently state doesn't save the new value in database
+        setting.set( ! value )
+    }
+    return <div className="control-content">
+        <IanControlHead 
+            label = { label }
+            description = { description }
         />
-    );
+
+        <div className="content-wrapper">
+            <FormToggle
+                checked = { value }
+                onChange = { handleChange }   // if possible create a seperate function
+            />
+        </div>
+    </div>
 };
 
 /**
  * TOD0: change file name to just toggle-button.js
  * TODO: used wp.components twice, use de-constructing (i already explained this to you, it's easy) 
  * TODO: Component name should be ToggleButtonComponent
- * TODO: Maintain spacing before and after "="
+ * TODO: Maintain spacing before and after "=" yo milauna paryo, ali uniform hunxa yesari
+ * TODO: IanControlHead adds label and description passed from php file
+ * 
+ * setting.get() gets the currently save database value
+ * setting.set( newValue ) updates the value in database
  */
